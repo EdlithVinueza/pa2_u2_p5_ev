98ae73c3ed78bd04f7285767d6d2feb947d15580
Edlith Vinueza <eavinuezaz1@uce.edu.ec>
2024-01-09 11:56:40 -0500

    mamytomany

--- src/main/java/com/uce/edu/Pa2U2P5EvApplication.java
+++ src/main/java/com/uce/edu/Pa2U2P5EvApplication.java
@@ -1,43 +1,20 @@
 package com.uce.edu;
 
-import java.awt.geom.Arc2D;
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-import org.aspectj.weaver.patterns.ThisOrTargetAnnotationPointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.repository.IAlumnoRepository;
-import com.uce.edu.repository.IAutorRepository;
-import com.uce.edu.repository.IEmpleadoRepository;
-import com.uce.edu.repository.modelo.Alumno;
-import com.uce.edu.repository.modelo.Autor;
 import com.uce.edu.repository.modelo.Autor2;
 import com.uce.edu.repository.modelo.AutorLibro;
-import com.uce.edu.repository.modelo.Ciudadano;
-import com.uce.edu.repository.modelo.Empleado;
-import com.uce.edu.repository.modelo.Estudiante;
-import com.uce.edu.repository.modelo.Habitacion;
-import com.uce.edu.repository.modelo.Hotel;
 import com.uce.edu.repository.modelo.Libro;
 import com.uce.edu.repository.modelo.Libro2;
-import com.uce.edu.service.IAlumnoService;
 import com.uce.edu.service.IAutorService;
-import com.uce.edu.service.ICiudadanoService;
-import com.uce.edu.service.IEmpleadoService;
-import com.uce.edu.service.IEstudianteService;
-import com.uce.edu.service.IHabitacionService;
-import com.uce.edu.service.IHotelService;
 import com.uce.edu.service.ILibroService;
-
-import jakarta.persistence.Id;
 
 @SpringBootApplication
 public class Pa2U2P5EvApplication implements CommandLineRunner {
